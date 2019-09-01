219022166b4d3e5dd013ba39e6567d7d3cccad7c
Rishabh-Joshi06 <rishabhjohi41@gmail.com>
2019-09-01 13:11:35 +0530

    Spinner added .....

--- app/src/main/java/com/example/engineeringnotes/MainActivity.java
+++ app/src/main/java/com/example/engineeringnotes/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.engineeringnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button notes;
+    Button setre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notes=(Button) findViewById(R.id.notes);
+        notes.setOnClickListener(this);
+        setre=(Button) findViewById(R.id.setremainder);
+        setre.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id=view.getId();
+
+        if (id==R.id.notes)
+        {
+            Intent intent=new Intent(this,notes.class) ;
+            startActivity(intent);
+        }
+        else if(id==R.id.setremainder)
+        {
+
+        }
+
     }
 }
